61061c86f36a73fad425a184a90a4e8fa865801f
Varma S <varma.surya@protonmail.com>
2024-02-29 18:14:56 -0500

    creating first react element using JSX

--- app.js
+++ app.js
@@ -1,59 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// Creating a react element using JSX
+//JSX is not part of react
 
-// creating a HTML element and rendering at id root
-const headingOne = React.createElement(
+// Using core react below is the creation of react element
+const firstElement = React.createElement(
   "h1",
-  { id: "headingOne", xyz: "abc" },
-  "Namaste React"
+  { id: "headingTwo" },
+  "Namstae React using Core react"
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(headingOne);
+const secondRoot = ReactDOM.createRoot(document.getElementById("root-two"));
+secondRoot.render(firstElement);
 
-// Creating a nested element and rendering at id root-two
-
-const parentElement = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm H1 Tag")
-  )
-);
-
-const rootElement = ReactDOM.createRoot(document.getElementById("root-two"));
-rootElement.render(parentElement);
-
-// ReactElement(Object) => HTML(browser rendering)
-
-// Now creating React elements nested with siblings at end
-
-const siblingElement = React.createElement(
-  "div",
-  { id: "parent-two" },
-  React.createElement("div", { id: "child-two" }, [
-    React.createElement("h1", {}, "Im a h1 Tag with sibling"),
-    React.createElement("h2", {}, "Im an h2 tag with a sibling"),
-  ])
-);
-
-const rootElementWithSibling = ReactDOM.createRoot(
-  document.getElementById("root-three")
-);
-rootElementWithSibling.render(siblingElement);
-
-// Now creating the same as above but double the number of children
-const finalElement = React.createElement("div", { id: "parent-final" }, [
-  React.createElement("div", { id: "child-final-one" }, [
-    React.createElement("h1", {}, "Im a h1 Tag with sibling"),
-    React.createElement("h2", {}, "Im an h2 tag with a sibling"),
-  ]),
-  React.createElement("div", { id: "child-final-two" }, [
-    React.createElement("h1", {}, "Im a h1 Tag with sibling"),
-    React.createElement("h2", {}, "Im an h2 tag with a sibling"),
-  ]),
-]);
-
-const rootFinal = ReactDOM.createRoot(document.getElementById("root-four"));
-rootFinal.render(finalElement);
+const jsxElement = (
+  <h1 id="headingOne">Welcome to Namastae React using JSX Syntax</h1>
+); //JSX SYNTAX to create a react element
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(jsxElement);
