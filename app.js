b20eaf5de6c170d9a358548abf5367418d1a8648
Varma S <varma.surya@protonmail.com>
2024-03-02 00:28:50 -0500

    Functional Components

--- app.js
+++ app.js
@@ -23,3 +23,45 @@ const jsxElement = (
 ); //JSX SYNTAX to create a react element
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 rootElement.render(jsxElement);
+
+// React Component
+// Class Based Components->>OLD WAY OF DOING
+// Functional Components->>NEW WAY OF DOING
+
+const FunctionalComponentOne = () => {
+  return (
+    <h1 className="functional-component">This is a Functional Component</h1>
+  );
+};
+const thirdRoot = ReactDOM.createRoot(document.getElementById("root-three"));
+
+thirdRoot.render(<FunctionalComponentOne />);
+
+// placing a  component into an other component is called Component Composition
+
+const FunctionalComponentTwo = () => {
+  return (
+    <div>
+      <h1>This is Second Functional Component</h1>
+      <FunctionalComponentOne />
+    </div>
+  );
+};
+const oneRoot = ReactDOM.createRoot(document.getElementById("root-four"));
+oneRoot.render(<FunctionalComponentTwo />);
+
+// Creating a Functional component using Normal javascript Function:
+const NormalComponent = function () {
+  return <h2>Normal Function</h2>;
+};
+
+// Functional Component Three-where we can insert javascript inside the JSX/or an React Element inside Componet
+const FunctionalComponentThree = () => (
+  <div>
+    <span>{jsxElement}</span>
+    <h3>{console.log("Hello-World")}</h3>
+    <h3>FUnctional component Three</h3>
+  </div>
+);
+
+oneRoot.render(<FunctionalComponentThree />);
